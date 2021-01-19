d5473f9df61780c5d67cad32cc16a9aedb241125
Tyler McCue <tjmcue@calpoly.edu>
2021-01-18 23:42:15 -0800

    change 0 coverage

--- bears.py
+++ bears.py
@@ -1,6 +1,7 @@
 # int -> T/F
 #Function that sees if a given integer can win the bear game
 def bears(n):
+    print(n)
     a = 0
     b = 0
     c = 0
@@ -18,7 +19,7 @@ def bears(n):
             if b == True:
                 return(True)
         if n % 3 == 0 or n % 4 == 0:
-            if '0' not in str(n):
+            if '0' not in str(n)[len(str(n))-2:-1]:
                 a = bears(n - int(float(str(n)[len(str(n))-2:-1]))*int(str(n)[-1]))
                 if a == True:
                     return(True)
@@ -26,3 +27,5 @@ def bears(n):
             return(False)
     if a == False or b == False or c == False:
         return(False)
+
+print(bears(1053))
