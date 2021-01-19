4eceeae9f2271d0b94bf7e5ac16218966b042780
Tyler McCue <tjmcue@calpoly.edu>
2021-01-18 23:52:53 -0800

    left a print in

--- perm_lex.py
+++ perm_lex.py
@@ -1,7 +1,6 @@
 # str -> list
 #This function will take a string and return a list of permutations of the str
 def perm_gen_lex(a):
-    print(a)
     if len(a) <= 1:
         return [a]
     list = []
